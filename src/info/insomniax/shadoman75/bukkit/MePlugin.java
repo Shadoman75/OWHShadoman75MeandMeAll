67b0532ccb8fce16c9d8385e7a3e57439b789a69
shadoman75 <shadowkiller77776@yahoo.com>
2013-10-12 17:36:59 -0500

    blah

--- src/info/insomniax/shadoman75/bukkit/MePlugin.java
+++ src/info/insomniax/shadoman75/bukkit/MePlugin.java
@@ -27,55 +27,44 @@ public final class MePlugin extends JavaPlugin {
 	}
 	public PermissionsHandler permissions = null;
 	
+	public boolean arrayToString(String[] args){
+		if(args.length > 0){
+			// ARGUMENT LOOP START!
+			StringBuilder buffer = new StringBuilder();
+			for(int i = 0; i < args.length; i++){
+				buffer.append(' ').append(args[i]);
+			}
+			// argument loop end...
+			return true;
+		}
+		return false;
+	}
 	public boolean onCommand(CommandSender sender, Command cmd,
 		String commandLabel, String[] args) {
 		
 		// if someone just happened to type /me and had a message after it...
 		if(cmd.getName().equalsIgnoreCase("me")){
 			if(permissions.has(sender, "meplugin.me")){
-			
-				if(args.length > 0){
-					// ARGUMENT LOOP START!
-					StringBuilder buffer = new StringBuilder();
-
-					for(int i = 0; i < args.length; i++)
-					{
-						buffer.append(' ').append(args[i]);
-					}
-					// argument loop end...
-				
+				if(arrayToString()){//placeholder for now
 					// PLAYER LOOPZ!
 					for(Player p:((Player)sender).getWorld().getPlayers()){
 						// and the message will be sent here :o
 						p.sendMessage(ChatColor.AQUA + "~" + " " + sender.getName() + buffer.toString());
-						
-					}
-					return true;
-				} 
-			}
-		}
-		return false;
-	}
-	public boolean onCommand1(CommandSender sender, Command cmd,
-		String commandLabel, String[] args) {
-		if(cmd.getName().equalsIgnoreCase("meall")){
-			if(permissions.has(sender, "meplugin.all")){
-				if(args.length > 0){
-					// ARGUMENT LOOP START!
-					StringBuilder buffer2 = new StringBuilder();
-
-					for(int i = 0; i < args.length; i++)
-					{
-						buffer2.append(' ').append(args[i]);
-					}
-					// argument loop end...
-					for(Player playr: getServer().getOnlinePlayers()){
-						playr.chat("/me" + buffer2.toString());
 					}
 					return true;
 				}
 			}
-		}
+		} else {
+		if(cmd.getName().equalsIgnoreCase("meall")){
+			if(permissions.has(sender, "meplugin.all")){
+				if(arraytoString()){ //placeholder for now
+					for(Player playr: getServer().getOnlinePlayers()){
+						playr.chat("/me" + buffer.toString());
+					}
+					return true;
+				}
+			}
+		}}
 		return false;
 	}
 
